2d793b52fe7c351a7d485097001383285122aa53
supakeen <cmdr@supakeen.com>
2021-01-02 14:22:27 +0100

    Update to draw temperatures.

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,127 @@
 #include <Arduino.h>
-#include <ArduinoOTA.h>
 
 #include <SPI.h>
 #include <WiFi.h>
 #include <Wire.h>
 #include <MQTT.h>
-#include <Button2.h>
 
 #include <GxEPD2_BW.h>
 
-#include <Fonts/FreeMonoBold9pt7b.h>
+#include <LineProtocol.h>
+
+#include <map>
+
+#include <Fonts/FreeSansBold9pt7b.h>
+#include "FreeSans7pt7b.h"
 
 #include "setting.h"
+
+#define every(t) for (static uint16_t _lasttime; (uint16_t)((uint16_t) millis() - _lasttime) >= (t); _lasttime = millis())
 
 #define MAX_DISPLAY_BUFFER_SIZE 800 
 #define MAX_HEIGHT(EPD) (EPD::HEIGHT <= MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8) ? EPD::HEIGHT : MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8))
 
 MQTTClient mqtt;
 
-Button2 btn1(BUTTON_1);
-Button2 btn2(BUTTON_2);
-
 GxEPD2_BW<GxEPD2_213_B73, GxEPD2_213_B73::HEIGHT> display(GxEPD2_213_B73(/*CS=5*/ 5, /*DC=*/ 17, /*RST=*/ 16, /*BUSY=*/ 4)); // GDEH0213B73
 
-const char HelloWorld[] = "Hello, RevSpace!";
+std::map<String, std::map<String, String>> state;
 
-void helloWorld()
-{
-  display.setRotation(1);
-  display.setFont(&FreeMonoBold9pt7b);
-  display.setTextColor(GxEPD_BLACK);
-  int16_t tbx, tby; uint16_t tbw, tbh;
-  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
-  // center the bounding box by transposition of the origin:
-  uint16_t x = ((display.width() - tbw) / 2) - tbx;
-  uint16_t y = ((display.height() - tbh) / 2) - tby;
-  display.setFullWindow();
-  display.firstPage();
-  do
-  {
-    display.fillScreen(GxEPD_WHITE);
-    display.setCursor(x, y);
-    display.print(HelloWorld);
+void dump_state() {
+  for(auto sensor: state) {
+      Serial.print(sensor.first + " ");
+
+      for(auto room: sensor.second) {
+        Serial.print(room.first + "=" + room.second + " ");
+      }
+
+      Serial.println();
   }
-  while (display.nextPage());
+}
+
+void draw_state() {
+    display.setFont(&FreeSansBold9pt7b);
+    display.setTextColor(GxEPD_BLACK);
+    display.setTextSize(0.75);
+    display.setFullWindow();
+
+    display.firstPage();
+    display.fillScreen(GxEPD_WHITE);
+
+    display.setCursor(0, 12);
+
+    display.print("Temperatures");
+
+    display.setFont(&FreeSans7pt7b);
+
+    int i = 30;
+
+    for(auto room: state["temperature"]) {
+      display.setCursor(0, i);
+      display.print(room.first + " " + String(room.second.toFloat(), 1));
+      i += 12;
+    }
+
+    display.nextPage();
+}
+
+void setup_display() {
+    display.init();
+    display.setRotation(0);
+
+    display.setFullWindow();
+    display.firstPage();
+    display.fillScreen(GxEPD_WHITE);
+    display.nextPage();
+}
+
+void setup_wifi() {
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED) delay(500);
+}
+
+void loop_wifi() {
+}
+
+void callback_mqtt(String &topic, String &payload) {
+    struct line_protocol message;
+
+    if(line_protocol_parse(message, payload)) {
+        return;
+    }
+
+    state[message.measurement][message.tags["room"]] = message.fields["value"];
+}
+
+void setup_mqtt() {
+    static WiFiClient wificlient;
+    mqtt.begin("192.168.1.10", 1883, wificlient);
+    mqtt.onMessage(callback_mqtt);
+
+    while(!mqtt.connect("")) delay(500);
+
+    mqtt.subscribe("/sensor/temperature");
+    mqtt.subscribe("/esp8266/temperature");
+}
+
+void loop_mqtt() {
+    mqtt.loop();
 }
 
 void setup() {
     Serial.begin(115200);
-    Serial.println("boot");
-    display.init();
-    helloWorld();
+
+    setup_display();
+    setup_wifi();
+    setup_mqtt();
 }
 
 void loop() {
+    loop_wifi();
+    loop_mqtt();
+
+    dump_state();
+
+    every(10000) draw_state();
 };
--- src/setting.h
+++ src/setting.h
@@ -5,29 +5,3 @@
 #ifndef WIFI_PASSWORD
 #define WIFI_PASSWORD "test"
 #endif
-
-#ifndef OTA_HOSTNAME
-#define OTA_HOSTNAME "test"
-#endif
-
-#ifndef OTA_PASSWORD
-#define OTA_PASSWORD "test"
-#endif
-
-#ifndef TFT_DISPOFF
-#define TFT_DISPOFF 0x28
-#endif
-
-#ifndef TFT_SLPIN
-#define TFT_SLPIN 0x10
-#endif
-
-#define TFT_BACKLIGHT 4
-#define ADC_EN 14
-#define ADC_PIN 34
-#define BUTTON_1 35
-#define BUTTON_2 0
-
-#define FONT_INTRO 2
-#define FONT 4
-#define FONT_ALARM 6
